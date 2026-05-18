a5151f7f669350c01006fb4173bb65c57189d180
adndrsd1 <adindarizki15@gmail.com>
2026-05-18 10:09:08 +0700

    fix github actions path

--- preprocessing/automate_Adinda Rizki Syabana Diva.py
+++ preprocessing/automate_Adinda Rizki Syabana Diva.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 def preprocessing():
-    data = pd.read_csv('Titanic-Dataset.csv')
+    data = pd.read_csv('../Titanic-Dataset.csv')
 
     data.drop(
         ['PassengerId', 'Name', 'Ticket', 'Cabin'], 
